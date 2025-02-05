849102c1c4e202885502c71d52fccdcca48fb4e8
eugenetangkj <dragoneugene10@gmail.com>
2025-02-05 11:13:37 +0800

    Add colours and font settings

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,10 +9,37 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        "yap-brown": {
+          100: "#E8CABA", 
+          900: "#8D5F4A"
+        },
+        "yap-orange": {
+          100: "#FFAF91",
+          900: "#FF814A", 
+        },
+        "yap-yellow": {
+          100: "#FEECC2", 
+          900: "#FFAF91"
+        },
+        "yap-green": {
+          100: "#CAD2B0", 
+          900: "#92A062"
+        },
+        "yap-gray": {
+          100: "#FAF4F4",
+          200: "#E2E2E2",
+          900: "84838A"
+        },
+        "yap-black": {
+          800: "3D3A39"
+        }
+      },
+      fontFamily: {
+        kavoon: "var(--font-kavoon)",
+        afacad: "var(--font-afacad)",
       },
     },
   },
   plugins: [],
+  darkMode: 'class',
 } satisfies Config;
