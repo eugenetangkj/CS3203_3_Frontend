a26f923f9a3c818a2618531f8aefbf8686e6e5cb
eugenetangkj <dragoneugene10@gmail.com>
2025-03-01 15:43:19 +0800

    Add sign up page

--- src/components/sign-up/SignUpForm.tsx
+++ src/components/sign-up/SignUpForm.tsx
@@ -1,10 +1,15 @@
 "use client"
+
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
 import { Button } from "@/components/ui/button"
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
+import { useState } from "react"
+import { Eye, EyeClosed } from "lucide-react"
+import { useToast } from "@/hooks/use-toast"
+import { ERROR_MESSAGE_API } from "@/constants/ConstantValues"
 
 /**
 This component represents the form for signing up a new account.
@@ -26,29 +31,85 @@ const formSchema = z.object({
               .regex(/[A-Z]/, { message: "Password must contain at least one uppercase letter." })
              .regex(/[a-z]/, { message: "Password must contain at least one lowercase letter." })
              .regex(/[0-9]/, { message: "Password must contain at least one number." })
-             .regex(/[^A-Za-z0-9]/, { message: "Password must contain at least one special character (!@#$%^&*)." })
-})
+             .regex(/[^A-Za-z0-9]/, { message: "Password must contain at least one special character (!@#$%^&*)." }),
+    
+    confirmPassword: z.string({ message: 'Password cannot be empty.' })
+                     .min(5, { message: 'Password must be a minimum of 5 characters long.'})
+                     .max(20, { message: 'Password must be a maximum of 20 characters long.'})
+                     .regex(/[A-Z]/, { message: "Password must contain at least one uppercase letter." })
+                     .regex(/[a-z]/, { message: "Password must contain at least one lowercase letter." })
+                     .regex(/[0-9]/, { message: "Password must contain at least one number." })
+                     .regex(/[^A-Za-z0-9]/, { message: "Password must contain at least one special character (!@#$%^&*)." }),
+}).superRefine(({ confirmPassword, password }, ctx) => {
+    if (confirmPassword !== password) {
+      ctx.addIssue({
+        code: "custom",
+        message: "The passwords did not match.",
+        path: ['confirmPassword']
+      });
+    }
+});
+
 
 
 export default function SignUpForm() {
-    // 1. Define your form.
+    //States
+    const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+    const [isConfirmPasswordVisible, setIsConfirmPasswordVisible] = useState(false);
+    const [isSubmittingForm, setIsSubmittingForm] = useState(false)
+
+
+    //Toggle password visibility
+    const togglePasswordVisibility = () => {
+        setIsPasswordVisible((prev: boolean) => !prev);
+    };
+    const toggleConfirmPasswordVisibility = () => {
+        setIsConfirmPasswordVisible((prev: boolean) => !prev);
+    };
+
+
+    //Toast management
+    const { toast } = useToast()
+
+
+
+
+    // Define the form
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
             name: "",
             email: "",
             password: "",
+            confirmPassword: ""
         },
     })
  
-    // 2. Define a submit handler.
-    function onSubmit(values: z.infer<typeof formSchema>) {
-        // Do something with the form values.
-        // ✅ This will be type-safe and validated.
-        console.log(values)
+    //Handler function after user presses sign up
+    function onSubmit({ name, email, password }: z.infer<typeof formSchema>) {
+        setIsSubmittingForm(true)
+
+        try {
+            // TODO: Make API call to register
+
+            //Successful, toast to inform the user that his account has been created, please login
+            toast({
+                variant: "success",
+                description: "Sign up is successful. Please login now.",
+                duration: 3000,
+            })
+            form.reset()
+        } catch (error) {
+            toast({
+                variant: "destructive",
+                description: ERROR_MESSAGE_API,
+                duration: 3000,
+            })
+        } finally {
+            // Clean up
+            setIsSubmittingForm(false)
+        }
     }
-
-
 
     return (
         <Form {...form}>
@@ -61,7 +122,7 @@ export default function SignUpForm() {
                 <FormItem>
                     <FormLabel className='text-yap-black-800 text-lg'>Name</FormLabel>
                     <FormControl>
-                        <Input placeholder="Your name" className='w-[400px] rounded-full text-base' {...field} />
+                        <Input placeholder="Your name" className='sign-up-field' {...field} />
                     </FormControl>
                   <FormMessage />
                 </FormItem>
@@ -76,7 +137,7 @@ export default function SignUpForm() {
                 <FormItem>
                     <FormLabel className='text-yap-black-800 text-lg'>Email</FormLabel>
                     <FormControl>
-                        <Input placeholder="Your email" type='email' className='w-[400px] rounded-full text-base' {...field} />
+                        <Input placeholder="Your email" type='email' className='sign-up-field' {...field} />
                     </FormControl>
                   <FormMessage />
                 </FormItem>
@@ -92,7 +153,12 @@ export default function SignUpForm() {
                 <FormItem>
                     <FormLabel className='text-yap-black-800 text-lg'>Password</FormLabel>
                     <FormControl>
-                        <Input placeholder="Your password" type='password' className='w-[400px] rounded-full text-base' {...field} />
+                        <div className='flex items-center space-x-2'>
+                            <Input placeholder="Your password" type={isPasswordVisible ? "text" : "password"} className='sign-up-field' {...field} />
+                            <div className='cursor-pointer text-yap-gray-800' onClick={togglePasswordVisibility}>
+                                { isPasswordVisible ? <EyeClosed /> : <Eye /> }
+                            </div>
+                        </div>
                     </FormControl>
                   <FormMessage />
                 </FormItem>
@@ -100,10 +166,33 @@ export default function SignUpForm() {
             />
 
 
+            {/* Confirm Password */}
+            <FormField
+              control={form.control}
+              name="confirmPassword"
+              render={({ field }) => (
+                <FormItem>
+                    <FormLabel className='text-yap-black-800 text-lg'>Confirm Password</FormLabel>
+                    <FormControl>
+                        <div className='flex items-center space-x-2'>
+                            <Input placeholder="Your password" type={isConfirmPasswordVisible ? "text" : "password"} className='sign-up-field' {...field} />
+                            <div className='cursor-pointer text-yap-gray-800' onClick={toggleConfirmPasswordVisibility}>
+                                { isConfirmPasswordVisible ? <EyeClosed /> : <Eye /> }
+                            </div>
+                        </div>
+                    </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+         
 
-
-
-            <Button className='rounded-full bg-yap-brown-900 hover:bg-yap-brown-800 duration-200 w-full text-base' type="submit">Sign Up</Button>
+            {/* Submit button */}
+            <Button className='rounded-full bg-yap-brown-900 hover:bg-yap-brown-800 duration-200 w-full text-base'
+                type="submit"
+                disabled={ isSubmittingForm }
+                
+            >{isSubmittingForm ? "Signing up..." : "Sign Up" }</Button>
           </form>
         </Form>
       )
