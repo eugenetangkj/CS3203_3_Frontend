b9416153bc4b1cf5a6dcae693678d35c9452d23f
eugenetangkj <dragoneugene10@gmail.com>
2025-03-09 17:22:00 +0800

    Remove homepage files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,3 @@
-import SearchBar from "@/components/home/SearchBar";
-import OrderByFilter from "@/components/home/OrderByFilter";
-import SelectSourceDropdown from "@/components/home/SelectSourceDropdown";
-import PostCard from "@/components/home/PostCard";
-import { posts } from "@/constants/posts";
-import { stringToSource } from "@/types/SocialMediaSource";
-import { stringToCategory } from "@/types/Category";
 import Image from "next/image";
 import Yappy from "../../public/graphics/yappy.svg";
 import PageTitle from "@/components/common/text/PageTitle";
@@ -22,23 +15,10 @@ export default function Home() {
           {/* Title */}
           <PageTitle pageTitle="What Singaporeans Are Yapping About" />
 
-          {/* Search and filter */}
-          <div className='flex flex-col space-y-4 lg:flex-row lg:justify-between lg:items-center lg:space-y-0'>
-            <SearchBar />
-            <div className='flex flex-row justify-start items-center space-y-0 space-x-4'>
-              <OrderByFilter />
-              <SelectSourceDropdown />
-            </div>
-          </div>
+          
         </div>
 
-        {/* Cards for posts */}
-        <div className='grid sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 grid-rows-1 gap-x-4 gap-y-4'>
-          {posts.map((post, index) => (
-            <PostCard key={index} title={post.title} category={ stringToCategory(post.category) }
-              description={post.description} date={post.date} source={stringToSource(post.source)} />
-          ))}
-        </div>
+       
 
         {/* Duck image */}
         <Image src={Yappy} alt="Yappy Duck" className="self-end w-36 h-36 sm:w-48 sm:h-48" />
--- src/types/Category.tsx
+++ src/types/Category.tsx
@@ -1,23 +1,3 @@
-export enum CategoryEnum {
-  Health = 'Health',
-  Education = 'Education',
-  Transport = "Transport",
-  Employment = "Employment",
-  Environment = "Environment",
-  Safety = "Safety",
-  Community = "Community",
-  Recreation = "Recreation",
-  Housing = "Housing",
-  Food = "Food",
-  Others = "Others"
-}
-
-export const stringToCategory= (input: string): CategoryEnum | undefined => {
-    return Object.values(CategoryEnum).includes(input as CategoryEnum) ? (input as CategoryEnum) : undefined;
-};
-  
-
-
 export interface Category {
     id: string,
     name: string,
