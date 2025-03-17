eb628d0c34908dfec3b30994558c05f6b8df81bf
eugenetangkj <dragoneugene10@gmail.com>
2025-03-17 10:36:05 +0800

    Fix accessibility issues

--- src/components/polls/view-polls/citizen/CitizenRewardPanel.tsx
+++ src/components/polls/view-polls/citizen/CitizenRewardPanel.tsx
@@ -21,9 +21,9 @@ export function CitizenRewardPanel({ isRewardPanelOpen, setIsRewardPanelOpen, co
                 <AlertDialogHeader>
                     <AlertDialogTitle className='text-xl text-yap-black-800'>Thanks for participating! Here&apos;s a reward.</AlertDialogTitle>
                 </AlertDialogHeader>
-                <div className="flex justify-center flex-col items-center">
-                    <p id='collectible-description' className='sr-only'>Here&apos;s a reward.</p>
-                    <ScratchCard width={300} height={150} finishPercent={40} brushSize={15}>
+                <div className="flex justify-center flex-col items-center space-y-2">
+                    <p id='collectible-description' className='sr-only'>Reward</p>
+                    <ScratchCard width={300} height={150} finishPercent={35} brushSize={15}>
                         <Image src={ collectiblePath } alt="Collectible" className="w-full h-full" height={150} width={300} />
                     </ScratchCard>
                 </div>
