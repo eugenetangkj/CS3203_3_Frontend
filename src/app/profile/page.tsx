f2ed7b37cad6d1b9829c833d9763ea6695cf0509
eugenetangkj <dragoneugene10@gmail.com>
2025-03-20 22:11:12 +0800

    Add create admin button in profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,9 +1,13 @@
 import PageTitle from "@/components/common/text/PageTitle";
 import ProfileCards from "@/components/profile/ProfileCards";
+import { User } from "@/types/User";
+import { getUserOidValue } from "@/utils/AuthChecker";
+import { API_BASE_URL_USER_MANAGEMENT, GET_PROFILE_BY_OID_ENDPOINT } from "@/constants/ApiRoutes";
+import axios from "axios";
 
 
 /** 
-Layout for the sign up page for new users to create an account
+Layout for the view profile page.
 */
 
 export const metadata = {
@@ -12,15 +16,57 @@ export const metadata = {
 };
 
 
-export default function Profile() {
+export default async function Profile() {
+    //Adapter function to convert API response to a User object
+    const mapResponseToUser = (profileData: any): User => {
+        return {
+            id: profileData._id.$oid,
+            email: profileData.email,
+            name: profileData.name,
+            role: profileData.role,
+            collectibles: profileData.collectibles
+        };
+    };
+
+    //Function to fetch the user information
+    const fetchUser = async () => {
+        try {
+            //Determine the user oid from cookies
+            const userOid = await getUserOidValue()
+            if (userOid.length === 0) {
+                return {}
+            }
+
+            //Fetch user data using oid value
+            const fetchUserProfileApiEndpoint = API_BASE_URL_USER_MANAGEMENT + '/' + GET_PROFILE_BY_OID_ENDPOINT
+            const userData = await axios.post(fetchUserProfileApiEndpoint,
+                {
+                    "oid": userOid
+                }
+            )
+            const currentUser = mapResponseToUser(userData.data)
+            return currentUser
+
+        } catch (error) {
+            return {}
+        }
+    }
+
+
+    //Get user information
+    const currentUser = await fetchUser()
+    
+
     return (
         <div className="px-6 md:px-12 font-afacad mt-32 mb-8">
             <div className="flex flex-col space-y-8">
                 {/* Title */}
                 <PageTitle pageTitle="Profile" />
-
-                {/* Profile information */}
-                <ProfileCards />
+                {
+                    (Object.keys(currentUser).length === 0)
+                    ? <div>Something went wrong. Please try again later.</div>
+                    : <ProfileCards currentUser={ currentUser as User } />
+                }
             </div>
         </div>
     );
--- src/components/profile/ProfileCards.tsx
+++ src/components/profile/ProfileCards.tsx
@@ -1,90 +1,37 @@
-"use client"
-
-import { useState, useEffect } from "react"
 import { User, UserRoleEnum } from "@/types/User"
-import { Skeleton } from "../ui/skeleton"
 import BasicInformationProfileCard from "./BasicInformationProfileCard";
 import CollectiblesProfileCard from "./CollectiblesProfileCard";
-import axios from "axios";
-import { API_BASE_URL_USER_MANAGEMENT, GET_PROFILE_BY_OID_ENDPOINT } from "@/constants/ApiRoutes";
-import { CHECK_USER_AUTH_SERVER_ENDPOINT } from "@/constants/ApiRoutes";
-
+import { Button } from "../ui/button";
 
 /** 
 This component represents the container for the profile cards to be displayed in the profile page.
 */
-export default function ProfileCards() {
-    //States
-    const [hasRanApi, setHasRanApi] = useState<boolean>(false)
-    const [isThereError, setIsThereError] = useState<boolean>(false)
-    const [currentUser, setCurrentUser] = useState<User>()
+interface ProfileCardsProps {
+    currentUser: User
+}
 
-
-
-    //Adapter function to convert API response to a User object
-    const mapResponseToUser = (profileData: any): User => {
-        return {
-            id: profileData._id.$oid,
-            email: profileData.email,
-            name: profileData.name,
-            role: profileData.role,
-            collectibles: []  //Just default set to empty array first
-        };
-    };
-
-
-    //Calls backend to fetch profile information
-    const fetchUser = async () => {
-        try {
-            //Call API
-            const response = await axios.post(CHECK_USER_AUTH_SERVER_ENDPOINT);
-            const userOid = response.data.userOid
-            const fetchUserProfileApiEndpoint = API_BASE_URL_USER_MANAGEMENT + '/' + GET_PROFILE_BY_OID_ENDPOINT
-            const userData = await axios.post(fetchUserProfileApiEndpoint,
-                {
-                    "oid": userOid
-                }
-            )
-           
-            const currentUser = mapResponseToUser(userData.data.profile)
-        
-            //Update state
-            setCurrentUser(currentUser)
-        } catch (error) {
-            setIsThereError(true)
-        } finally {
-            setHasRanApi(true)
-        }
-    }
-
-
-    //Fetch profile information on component mount
-    useEffect(() => {
-        fetchUser()
-    }, [])
-
-
-
+export default async function ProfileCards({ currentUser }: ProfileCardsProps) {
     return (
-        !hasRanApi
-        ? (<Skeleton className="w-full h-[200px]" />)
-        : isThereError
-        ? <div>Something went wrong. Please try again later.</div>
-        : (
+        <div className='flex flex-col items-center space-y-6'>
+            {/* Add admin account button */}
+            {
+                currentUser.role === UserRoleEnum.Admin &&
+                <a className='self-end' href='/create-admin'>
+                    <Button className="rounded-full bg-yap-orange-900 hover:bg-yap-orange-800 duration-200 text-white text-base">Create Admin Account</Button>
+                </a>
+            }
+
+            {/* Profile cards */}
             <div className='grid grid-rows-1 grid-cols-1 lg:grid-cols-6 w-full gap-y-4 lg:gap-x-8 lg:gap-y-0'>
-                <BasicInformationProfileCard user={ currentUser }/>
+                <BasicInformationProfileCard user={ currentUser as User }/>
 
                 {/* Only display collectibles for citizen */}
                 {
                     currentUser?.role && currentUser?.role === UserRoleEnum.Citizen && <CollectiblesProfileCard user={ currentUser } />
                 }
             </div>
-        )      
+        </div>
+        
+        
     )
-   
-
-
-
-
-
 }
