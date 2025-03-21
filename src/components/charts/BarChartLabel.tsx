b0bcc466adb1a38a884383b12b5c2dd355aa6902
eugenetangkj <dragoneugene10@gmail.com>
2025-03-21 16:12:16 +0800

    Adjust bar chart labels

--- src/components/charts/BarChartLabel.tsx
+++ src/components/charts/BarChartLabel.tsx
@@ -4,16 +4,10 @@ import { Bar, BarChart, CartesianGrid, LabelList, XAxis } from "recharts"
 import { ChartConfig, ChartContainer, ChartTooltip, ChartTooltipContent, } from "@/components/ui/chart"
 import { COLOUR_MAP } from "@/constants/Constants"
 import { BarChartLabelPoint } from "@/types/ChartInterface"
+import { BREAKPOINTS } from "@/constants/Constants"
+import { useState, useEffect } from "react"
 
 
-const chartData = [
-  { month: "January", desktop: 186 },
-  { month: "February", desktop: 305 },
-  { month: "March", desktop: 237 },
-  { month: "April", desktop: 73 },
-  { month: "May", desktop: 209 },
-  { month: "June", desktop: 214 },
-]
 
 /**
 A bar chart with label visualisation
@@ -33,14 +27,49 @@ export function BarChartLabel({ chartData }: BarChartLabelProps) {
     const xKey = keys[0];
     const yKey = keys[1];
 
+    //Set number of characters to show
+    const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+    const [numberOfCharactersToShow, setNumberOfCharactersToShow] = useState<number>(10); 
+    // Adjust scaling factor based on screen size
+    useEffect(() => {
+        const handleResize = () => {
+            setScreenWidth(window.innerWidth);
+        };
+        window.addEventListener('resize', handleResize);
 
+        // Cleanup listener on unmount
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+     
+    useEffect(() => {
+        // Change maxValue based on screen size
+        if (screenWidth <= BREAKPOINTS['xs']) {
+            setNumberOfCharactersToShow(3);
+        } else if (screenWidth <= BREAKPOINTS['sm']) {
+            setNumberOfCharactersToShow(3);
+        } else if (screenWidth <= BREAKPOINTS['md']) {
+            setNumberOfCharactersToShow(5); 
+        } else if (screenWidth <= BREAKPOINTS['lg']) {
+            setNumberOfCharactersToShow(10); 
+        } else if (screenWidth <= BREAKPOINTS['xl']) {
+            setNumberOfCharactersToShow(20); 
+        } else if (screenWidth <= BREAKPOINTS['2xl']) {
+            setNumberOfCharactersToShow(30); 
+        } else {
+            setNumberOfCharactersToShow(-1); //Show original 
+        }
+        }, [screenWidth]);
+
+
+
+        
     return (
             <ChartContainer config={chartConfig} className='max-h-[200px] w-full'>
                 <BarChart
                     accessibilityLayer
                     data={chartData}
                     margin={{
-                    top: 20,
+                    top: 40,
                     }}
                 >
                     <CartesianGrid vertical={false} />
@@ -50,7 +79,9 @@ export function BarChartLabel({ chartData }: BarChartLabelProps) {
                     tickMargin={10}
                     axisLine={false}
                     className='text-sm md:text-base text-yap-black-800'
-                    
+                    tickFormatter={(value) =>
+                        numberOfCharactersToShow !== -1  ? `${value.slice(0, numberOfCharactersToShow)}...` : value
+                    }
                     />
                     <ChartTooltip
                     cursor={false}
