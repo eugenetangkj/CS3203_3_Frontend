c9ba04cf6ebd24458801ba774654dd3c1768fef9
eugenetangkj <dragoneugene10@gmail.com>
2025-03-25 21:31:54 +0800

    Update sign in form

--- src/components/sign-in/SignInForm.tsx
+++ src/components/sign-in/SignInForm.tsx
@@ -9,12 +9,13 @@ import { Input } from "@/components/ui/input"
 import { useState } from "react"
 import { Eye, EyeClosed } from "lucide-react"
 import { useToast } from "@/hooks/use-toast"
-import { ERROR_MESSAGE_API, NO_MATCHING_DOCUMENTS_API_ERROR_MESSAGE } from "@/constants/Constants"
+import { ERROR_MESSAGE_API, NO_MATCHING_DOCUMENTS_API_ERROR_MESSAGE, SUCCESS } from "@/constants/Constants"
 import { confirmPasswordFieldValidation, emailFieldValidation } from "@/utils/FormValidation"
 import { useRouter } from "next/navigation";
 import { API_BASE_URL_USER_MANAGEMENT, LOGIN_ENDPOINT, SIGNIN_SERVER_ENDPOINT } from "@/constants/ApiRoutes";
 import axios from "axios"
 import { useAuth } from "@/context/AuthContext"
+import { setCookiesForSigningIn, userLogin } from "@/controllers/UsersController"
 
 /**
 This component represents the form for signing in an existing account
@@ -57,51 +58,34 @@ export default function SignInForm() {
     async function onSubmit({ email, password }: z.infer<typeof formSchema>) {
         setIsSubmittingForm(true)
 
-        try {
-            //Make API call to login
-            const loginApiEndpoint = API_BASE_URL_USER_MANAGEMENT  + LOGIN_ENDPOINT
-            const apiResult = await axios.post(loginApiEndpoint,
-                {
-                    "email": email,
-                    "password": password
-                }
-            )
+        //Make API call to login
+        const loginResult = await userLogin(email, password)
 
-            //Check if login is successful
-            if (apiResult.data.message === NO_MATCHING_DOCUMENTS_API_ERROR_MESSAGE) {
-                toast({
-                    variant: "destructive",
-                    description: "Invalid credentials. Please check your email and/or password.",
-                    duration: 3000,
-                })
-            } else {
-                const jwtToken = apiResult.data.jwt
-                const userOid = apiResult.data.oid
-    
-                if (jwtToken) {
-                    //Set cookie with JWT token
-                    await axios.post(SIGNIN_SERVER_ENDPOINT, {
-                        "token": jwtToken,
-                        "userOid": userOid
-                    })
-    
-                    //Update global state
-                    login()
-    
-                    //Successful, redirect to home page
-                    router.push('/')
-                }
-            }
-        } catch (error) {
-            //Display destructive toast
+        //Check successful or not
+        if (loginResult.message !== SUCCESS) {
+            //CASE 1: Login is not successful
             toast({
                 variant: "destructive",
-                description: ERROR_MESSAGE_API,
+                description: loginResult.message,
                 duration: 3000,
             })
-        } finally {
-            // Clean up
             setIsSubmittingForm(false)
+        } else {
+            //CASE 2: Login is successful
+            const jwtToken = loginResult.jwt
+            const userOid = loginResult.oid
+    
+            //Set cookie with JWT token
+            await setCookiesForSigningIn(jwtToken, userOid)
+
+            //Update global state
+            login()
+
+            //Update form state
+            setIsSubmittingForm(false)
+
+            //Successful, redirect to home page
+            router.push('/')
         }
     }
 
--- src/controllers/UsersController.tsx
+++ src/controllers/UsersController.tsx
@@ -1,11 +1,13 @@
 "use server"
 
-import { API_BASE_URL_USER_MANAGEMENT, SIGNUP_ENDPOINT } from "@/constants/ApiRoutes";
-import { ERROR_MESSAGE_API, SUCCESS } from "@/constants/Constants";
+import { API_BASE_URL_USER_MANAGEMENT, LOGIN_ENDPOINT, SIGNUP_ENDPOINT } from "@/constants/ApiRoutes";
+import { ERROR_MESSAGE_API, SUCCESS, NO_MATCHING_DOCUMENTS_API_ERROR_MESSAGE } from "@/constants/Constants";
 import axios from "axios";
+import { COOKIE_JWT_TOKEN, COOKIE_USER_OID } from "@/constants/Constants";
+import { cookies } from "next/headers";
 
 
-//Fetch count
+//Sign up for a new account
 export const userSignUp = async (name: string, email: string, password: string, role: string, collectibles: string[]) => {
     try {
         const signUpEndpoint = API_BASE_URL_USER_MANAGEMENT + SIGNUP_ENDPOINT
@@ -17,9 +19,6 @@ export const userSignUp = async (name: string, email: string, password: string, 
                 "role": role,
                 "collectibles": collectibles
             },
-            headers: {
-            'Content-Type': 'application/json', 
-            }
         })
         return SUCCESS
     } catch (error) {
@@ -32,3 +31,50 @@ export const userSignUp = async (name: string, email: string, password: string, 
         }
     }
 };
+
+//Sign up for a new account
+export const userLogin = async (email: string, password: string) => {
+    try {
+        const loginEndpoint = API_BASE_URL_USER_MANAGEMENT + LOGIN_ENDPOINT
+        const loginResult = await axios.post(loginEndpoint, {
+                "email": email,
+                "password": password,
+        })
+        if (loginResult.data.message === NO_MATCHING_DOCUMENTS_API_ERROR_MESSAGE) {
+            return {
+                "message": "Invalid credentials. Please check your email and/or password.",
+                "jwt": "",
+                "oid": "",
+            }
+        } else {
+            return {
+                "message": SUCCESS,
+                "jwt": loginResult.data.jwt,
+                "oid": loginResult.data.oid
+            }
+        }
+    } catch (error) {
+        return {
+            "message": ERROR_MESSAGE_API,
+            "jwt": "",
+            "oid": "",
+        }
+    }
+};
+
+//Set cookies after signing in
+export const setCookiesForSigningIn = async (jwtToken: string, userOid: string) => {
+    const cookieStore = await cookies()
+    cookieStore.set({
+        name: COOKIE_JWT_TOKEN,
+        value: jwtToken,
+        httpOnly: true,
+        path: '/',
+    })
+    cookieStore.set({
+        name: COOKIE_USER_OID,
+        value: userOid,
+        httpOnly: true,
+        path: '/',
+    })
+}
