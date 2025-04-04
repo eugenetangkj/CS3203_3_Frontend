ceb990874121add7af6867a4690af4bd75cc6f9e
eugenetangkj <dragoneugene10@gmail.com>
2025-04-04 15:12:18 +0800

    Update email field validation

--- src/utils/FormValidation.tsx
+++ src/utils/FormValidation.tsx
@@ -6,18 +6,19 @@ This file contains the validation rules for sign up and sign in form.
 */
 
 //Validate name field
-export const nameFieldValidation = z.string({ message: 'Name cannot be empty.' })
+export const nameFieldValidation = z.string({ message: 'Name must be a string.' })
                                    .max(20, { message: 'Name must be a maximum of 20 characters long.' })
                                    .refine((val) => val.trim().length >= 2, { message: "Name must be a minimum of 2 characters long." })
 
 
 //Validate email field
-export const emailFieldValidation = z.string({ message: 'Email cannot be empty.'})
-                                    .email({ message: 'Please enter a valid email address.' })
+export const emailFieldValidation = z.string({ message: 'Email must be a string.' })
+                                    .min(1, { message: "Email cannot be empty." })
+                                    .email({ message: "Please enter a valid email address." });
 
 
 //Validate password field
-export const passwordFieldValidation = z.string({ message: 'Password cannot be empty.' })
+export const passwordFieldValidation = z.string({ message: 'Password must be a string.' })
                                        .min(5, { message: 'Password must be a minimum of 5 characters long.'})
                                        .max(20, { message: 'Password must be a maximum of 20 characters long.'})
                                        .regex(/[A-Z]/, { message: "Password must contain at least one uppercase letter." })
