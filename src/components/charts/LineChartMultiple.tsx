7c368125214ee62358060cbdc0ad4688a675755e
eugenetangkj <dragoneugene10@gmail.com>
2025-04-14 13:00:37 +0800

    Fix category analytics graphs

--- src/components/charts/LineChartMultiple.tsx
+++ src/components/charts/LineChartMultiple.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { CartesianGrid, Line, LineChart, XAxis, YAxis } from "recharts"
+import { CartesianGrid, Line, LineChart, ResponsiveContainer, XAxis, YAxis } from "recharts"
 import {
   ChartConfig,
   ChartContainer,
@@ -22,39 +22,43 @@ export function LineChartMultiple({ chartData, colourMap }: LineChartMultiplePro
 
     return (
         <ChartContainer config={chartConfig} className='max-h-[400px] w-full'>
-            <LineChart
-                accessibilityLayer
-                data={chartData}
-                margin={{
-                left: 0,
-                right: 12,
-                }}
-            >
-                <CartesianGrid vertical={true} />
-                    <YAxis domain={['auto', (max: number) => (max == 0) ? 1.05 : max * 1.05]} />
-                    <XAxis
-                        dataKey="date"
-                        tickLine={false}
-                        axisLine={false}
-                        tickMargin={8} 
-                        padding={{left: 7}}
-                    />
-                    <ChartTooltip cursor={false} content={<ChartTooltipContent />} />
+            <div className='w-full h-[400px]'>
+                <ResponsiveContainer width="100%" height="100%">
+                    <LineChart
+                        accessibilityLayer
+                        data={chartData}
+                        margin={{
+                        left: 0,
+                        right: 12,
+                        }}
+                    >
+                        <CartesianGrid vertical={true} />
+                            <YAxis domain={['auto', (max: number) => (max == 0) ? 1.05 : max * 1.05]} />
+                            <XAxis
+                                dataKey="date"
+                                tickLine={false}
+                                axisLine={false}
+                                tickMargin={8} 
+                                padding={{left: 7}}
+                            />
+                            <ChartTooltip cursor={false} content={<ChartTooltipContent />} />
 
-                    {Object.entries(colourMap).map(([category, colour]) => (
-                        <Line
-                            key={category}
-                            dataKey={category}
-                            type="monotone"
-                            stroke={colour}
-                            strokeWidth={2}
-                            dot={false}
-                        />
-                    ))}
+                            {Object.entries(colourMap).map(([category, colour]) => (
+                                <Line
+                                    key={category}
+                                    dataKey={category}
+                                    type="monotone"
+                                    stroke={colour}
+                                    strokeWidth={2}
+                                    dot={false}
+                                />
+                            ))}
 
 
-                    
-            </LineChart>
-            </ChartContainer>
+                            
+                    </LineChart>
+                </ResponsiveContainer>
+            </div>
+        </ChartContainer>
     )
 }
